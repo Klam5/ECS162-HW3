ef82c9fcbc8641e4ed8ca4677b905aa3502946cd
Kyle Lam <87782287+Klam5@users.noreply.github.com>
2025-05-17 17:41:06 -0700

    imported hw2 code into hw3 files

--- hw3-app-html/app.py
+++ hw3-app-html/app.py
@@ -58,3 +58,51 @@ def logout():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
+
+
+
+
+# https://flask.palletsprojects.com/en/stable/quickstart/
+# https://flask.palletsprojects.com/en/stable/api/#flask.jsonify
+# https://developer.nytimes.com/docs/articlesearch-product/1/overview
+
+# from flask import Flask, render_template, jsonify #Imports flask, render template for HTML file, jsonify to send JSON back and forth
+# import os       #used to read env variables 
+# import requests #sends requests
+# import json
+# from dotenv import load_dotenv  #loads env file
+
+# load_dotenv()
+# app = Flask(__name__)                       #Creates Flask app
+# NYT_API_KEY = os.getenv("NYT_API_KEY")      #gets api key from .env
+
+# @app.route('/')                             #Sets up root URL
+# def home():
+#     return render_template("index.html")    #renders index.html template
+
+# @app.route('/api/news')                     #apinews route used by js
+# def news():
+#     #Base url for NYT article searchapi
+#     url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+#     #Search Paramaters to control article searching 
+#     searchparams = {
+#         #Filter query to control location to Davis or Sacramento only 
+#         "fq": "timesTag.location.contains:(Davis OR Sacramento)",
+#         #Uses API key we received from env earlier
+#         "api-key": NYT_API_KEY,
+#         #Sort by recency
+#         "sort": "newest"
+#     }
+#     #Get request to API with the parameters to get article data
+#     response = requests.get(url, params=searchparams)
+#     #Returns jsonified version of data
+#     return jsonify(response.json())
+
+# @app.route('/api/key')
+# def get_api_key():
+#     return jsonify({"key": NYT_API_KEY})
+
+
+# #If main, run flask server
+# if __name__ == "__main__":
+#     app.run(port=8000, debug=True)
